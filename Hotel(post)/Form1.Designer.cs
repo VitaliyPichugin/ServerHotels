29ae1757c132c4ea93b678f032a35b21b9fc853d
Vitaliy Pichugin <vitaliypichugin92@gmail.com>
2018-01-16 01:46:25 +0200

    get report

---
+++ Hotel(post)/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Hotel_post_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(430, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 240);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(430, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Get report 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(454, 344);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Opacity = 0.5D;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Hotel(post)/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_post_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a request using a URL that can receive a post.   
+            WebRequest request = WebRequest.Create("http://hotels.t.zp.ua/reporttest/");
+            // Set the Method property of the request to POST.  
+            request.Method = "POST";
+          
+            // Create POST data and convert it to a byte array.  
+            string postData = "This is a test that posts this string to a Web server.";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            // Set the ContentType property of the WebRequest.  
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.  
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.  
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.  
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.  
+            dataStream.Close();
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+           // textBox1.Text = ((HttpWebResponse)response).StatusDescription;
+            // Get the stream containing content returned by the server.  
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.  
+           
+           
+            // Clean up the streams.  
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebRequest myHttpWebRequest = WebRequest.Create("http://hotels.t.zp.ua/reporttest/");
+            //  HttpWebRequest myHttpWebRequest = new HttpWebRequest;
+            // Set the 'Method' property of the 'Webrequest' to 'POST'.
+            myHttpWebRequest.Method = "POST";
+          //  Console.WriteLine("\nPlease enter the data to be posted to the (http://www.contoso.com/codesnippets/next.asp) Uri :");
+
+            // Create a new string object to POST data to the Url.
+           // string inputData = Console.ReadLine();
+
+
+            string postData = "firstone=1";
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] byte1 = encoding.GetBytes(postData);
+
+            // Set the content type of the data being posted.
+            myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+
+            // Set the content length of the string being posted.
+            myHttpWebRequest.ContentLength = byte1.Length;
+
+            Stream newStream = myHttpWebRequest.GetRequestStream();
+
+            newStream.Write(byte1, 0, byte1.Length);
+          //  Console.WriteLine("The value of 'ContentLength' property after sending the data is {0}", myHttpWebRequest.ContentLength);
+
+            // Close the Stream object.
+            newStream.Close();
+        }
+    }
+}
